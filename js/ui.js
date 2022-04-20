f6ea43f6a6a27a09842629b5219fac3bd9b2805d
RzayevTaleh01 <taleh.rzayev.i@asoiu.edu.az>
2022-04-20 14:48:02 +0400

    add random repo

--- js/ui.js
+++ js/ui.js
@@ -58,11 +58,15 @@ class UI {
     if (repos.length == 0) {
       this.repoDiv.innerHTML = `<p class="empty-repo">${repos.login} doesn't have any public repositories yet.</p>`;
     } else {
-      for (let i = 0; i < 3; i++) {
+      let random = Math.floor(Math.random() * repos.length);
+      console.log("random: " + random);
+      for (let i = 0; i < 4; i++) {
+        let index = i + random;
+        console.log("index: " + index);
         this.repoDiv.innerHTML += ` <div class="repo-item">
       <div class="repo-head">
-        <h1><a href="${repos[i].html_url}">${repos[i].name}</a></h1>
-        <p>${repos[i].description}</p>
+        <h1><a href="${repos[index].html_url}">${repos[index].name}</a></h1>
+        <p>${repos[index].description}</p>
       </div>
 
       <div class="repo-icons">
@@ -82,7 +86,7 @@ class UI {
                 d="M8 .25a.75.75 0 01.673.418l1.882 3.815 4.21.612a.75.75 0 01.416 1.279l-3.046 2.97.719 4.192a.75.75 0 01-1.088.791L8 12.347l-3.766 1.98a.75.75 0 01-1.088-.79l.72-4.194L.818 6.374a.75.75 0 01.416-1.28l4.21-.611L7.327.668A.75.75 0 018 .25zm0 2.445L6.615 5.5a.75.75 0 01-.564.41l-3.097.45 2.24 2.184a.75.75 0 01.216.664l-.528 3.084 2.769-1.456a.75.75 0 01.698 0l2.77 1.456-.53-3.084a.75.75 0 01.216-.664l2.24-2.183-3.096-.45a.75.75 0 01-.564-.41L8 2.694v.001z"
               ></path>
             </svg>
-            <strong>${repos[i].stargazers_count}</strong>
+            <strong>${repos[index].stargazers_count}</strong>
           </li>
           <li>
             <svg
@@ -99,7 +103,7 @@ class UI {
                 d="M1.679 7.932c.412-.621 1.242-1.75 2.366-2.717C5.175 4.242 6.527 3.5 8 3.5c1.473 0 2.824.742 3.955 1.715 1.124.967 1.954 2.096 2.366 2.717a.119.119 0 010 .136c-.412.621-1.242 1.75-2.366 2.717C10.825 11.758 9.473 12.5 8 12.5c-1.473 0-2.824-.742-3.955-1.715C2.92 9.818 2.09 8.69 1.679 8.068a.119.119 0 010-.136zM8 2c-1.981 0-3.67.992-4.933 2.078C1.797 5.169.88 6.423.43 7.1a1.619 1.619 0 000 1.798c.45.678 1.367 1.932 2.637 3.024C4.329 13.008 6.019 14 8 14c1.981 0 3.67-.992 4.933-2.078 1.27-1.091 2.187-2.345 2.637-3.023a1.619 1.619 0 000-1.798c-.45-.678-1.367-1.932-2.637-3.023C11.671 2.992 9.981 2 8 2zm0 8a2 2 0 100-4 2 2 0 000 4z"
               ></path>
             </svg>
-            <strong>${repos[i].watchers_count}</strong>
+            <strong>${repos[index].watchers_count}</strong>
           </li>
           <li>
             <svg
@@ -116,7 +120,7 @@ class UI {
                 d="M5 3.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm0 2.122a2.25 2.25 0 10-1.5 0v.878A2.25 2.25 0 005.75 8.5h1.5v2.128a2.251 2.251 0 101.5 0V8.5h1.5a2.25 2.25 0 002.25-2.25v-.878a2.25 2.25 0 10-1.5 0v.878a.75.75 0 01-.75.75h-4.5A.75.75 0 015 6.25v-.878zm3.75 7.378a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm3-8.75a.75.75 0 100-1.5.75.75 0 000 1.5z"
               ></path>
             </svg>
-            <strong>${repos[i].forks_count}</strong>
+            <strong>${repos[index].forks_count}</strong>
           </li>
         </ul>
       </div>
@@ -133,7 +137,8 @@ class UI {
     div.className = "error-message";
     p.textContent = message;
     div.appendChild(p);
-    this.formArticle.appendChild(div);
+    if(message != "TypeError: Cannot read properties of undefined (reading 'html_url')"){
+    this.formArticle.appendChild(div);}
     setTimeout(() => {
       div.remove();
     }, 9000);
